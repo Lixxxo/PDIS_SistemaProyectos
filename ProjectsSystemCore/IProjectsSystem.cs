aa73cf3c6722a14ecbd77eccc2064a2c0e2181c0
Guetti <61527120+Guetti@users.noreply.github.com>
2022-10-18 23:20:38 -0300

    feat: add system requierement

---
+++ ProjectsSystemCore/IProjectsSystem.cs
@@ -0,0 +1,36 @@
+﻿using SistemaProyectos.Model;
+
+namespace SistemaProyectos.ProjectsSystemCore;
+
+public interface IProjectsSystem
+{
+    /// <summary>
+    /// Create a project into the database.
+    /// </summary>
+    /// <param name="project">The new project.</param>
+    /// <returns>True if success.</returns>
+    bool CreateProject(Project project);
+
+    /// <summary>
+    /// Create a job (task) into the database.
+    /// </summary>
+    /// <param name="job">The new job (task).</param>
+    /// <returns></returns>
+    bool CreateJob(Job job);
+
+    /// <summary>
+    /// Modify an existent job (task) from the database.
+    /// </summary>
+    /// <param name="jobId">The id of the job to modify.</param>
+    /// <param name="updatedJob">The new job with updated values.</param>
+    /// <returns>True if success.</returns>
+    bool ModifyJob(int jobId, Job updatedJob);
+
+    /// <summary>
+    /// Assign job material to the given material.
+    /// </summary>
+    /// <param name="material">The focus material.</param>
+    /// <param name="jobMaterial">The job material that is going the assign.</param>
+    /// <returns>True if success.</returns>
+    bool AssignMaterial(Material material, JobMaterial jobMaterial);
+}
---
+++ ProjectsSystemCore/ProjectsSystem.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using SistemaProyectos.Model;
+using NotImplementedException = System.NotImplementedException;
+
+namespace SistemaProyectos.ProjectsSystemCore;
+
+/// <summary>
+/// System to administrate projects database.
+/// </summary>
+public class ProjectsSystem : IProjectsSystem
+{
+    /// <summary>
+    /// List of jobs.
+    /// </summary>
+    private List<Job> _jobs;
+
+    /// <summary>
+    /// List of projects.
+    /// </summary>
+    private List<Project> _projects;
+
+    /// <summary>
+    /// Constructor of the system.
+    /// </summary>
+    public ProjectsSystem()
+    {
+        _jobs = new List<Job>();
+        _projects = new List<Project>();
+    }
+
+    public bool CreateProject(Project project)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool CreateJob(Job job)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool ModifyJob(int jobId, Job updatedJob)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool AssignMaterial(Material material, JobMaterial jobMaterial)
+    {
+        throw new NotImplementedException();
+    }
+}
