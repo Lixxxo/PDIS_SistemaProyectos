8cee0b042e177cf0c9ad9a8fff2ed945ed6cf0ab
Guetti <61527120+Guetti@users.noreply.github.com>
2022-10-19 01:12:26 -0300

    refactor: reorder entities and namespace

--- Database/SystemDbContext.cs
+++ Database/SystemDbContext.cs
@@ -1,22 +1,29 @@
 using Microsoft.EntityFrameworkCore;
 using SistemaProyectos.Model;
 
-namespace SistemaProyectos.Database
-{
-    public class SystemDbContext : DbContext
-    { 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionBuilder)
+namespace SistemaProyectos.Database;
+
+/// <summary>
+/// Core of the database.
+/// </summary>
+public class SystemDbContext : DbContext
+{ 
+    protected override void OnConfiguring(DbContextOptionsBuilder optionBuilder)
+    {
+        if (!optionBuilder.IsConfigured)
         {
-            if (!optionBuilder.IsConfigured)
-            {
-                optionBuilder.UseSqlServer(DatabasePath.databasePath, 
-                    option => {});
-            }
+            optionBuilder.UseSqlServer(DatabasePath.databasePath, 
+                option => {});
         }
-        public virtual DbSet<Material> Materials { get; set; }
-        public virtual DbSet<Project> Projects { get; set; }
-        public virtual DbSet<Job> Jobs { get; set; }
-        public virtual DbSet<JobMaterial> JobMaterials { get; set; }
-        public virtual DbSet<Movement> Movements { get; set; }
     }
+
+    #region ENTITIES
+    
+    public virtual DbSet<Material> Materials { get; set; }
+    public virtual DbSet<Project> Projects { get; set; }
+    public virtual DbSet<Job> Jobs { get; set; }
+    public virtual DbSet<JobMaterial> JobMaterials { get; set; }
+    public virtual DbSet<Movement> Movements { get; set; }
+
+    #endregion
 }
