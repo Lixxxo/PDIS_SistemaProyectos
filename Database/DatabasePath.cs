61139c6e301531bdae912d455e9a08646b4d16b8
Lixxxo <luis.rojas04@alumnos.ucn.cl>
2022-10-19 00:53:17 -0300

    feat: connection string for aws db

--- Database/DatabasePath.cs
+++ Database/DatabasePath.cs
@@ -2,5 +2,6 @@ namespace SistemaProyectos.Database;
 
 public class DatabasePath
 {
-    public const string databasePath = "Server=LENOVO-GUETTI\\SQLEXPRESS;Database=pdis;Trusted_Connection=True;";
+    // public const string databasePath = "Server=LIXOACER\\SQLEXPRESS;Database=pdis;Trusted_Connection=True;";
+    public const string databasePath = "Server=pdisdb.cpjbcw2lpm85.us-east-1.rds.amazonaws.com;Database=pdisworkshop2;Uid=pdis;Pwd=G4Jt6FkiSn;";
 }
