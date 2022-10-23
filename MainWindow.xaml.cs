a610e230fc63724c3e7d842619a465723379fa6a
Lixxxo <luis.rojas04@alumnos.ucn.cl>
2022-10-23 18:28:31 -0300

    feat: add task wth bugs

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -10,6 +10,7 @@ using SistemaProyectos.Database;
 using SistemaProyectos.Model;
 using System.Data;
 using System.Data.Entity.Core.Objects;
+using System.Windows.Input;
 
 namespace SistemaProyectos
 {
@@ -21,11 +22,13 @@ namespace SistemaProyectos
         List<Project>? ProjectList { get; set; }
         List<Task>? TaskList { get; set; }
         Project SelectedProject { get; set; }
+        Task SelectedTask { get; set; }
         
         public MainWindow()
         {
             InitializeComponent();
-
+            BtnCreateTask.IsEnabled = false;
+            BtnAssignMaterial.IsEnabled = false;
             PopulateView();
         }
 
@@ -37,42 +40,89 @@ namespace SistemaProyectos
                 context.Projects.Add(p);
                 context.SaveChanges();
             }
+
+            this.SelectedProject = p;
             PopulateView();
+
         }
+        
         private void BtnCreateTask_Click(object sender, RoutedEventArgs e)
         {
-            var t = new Task
-            {
-                Project = SelectedProject
-            };
+            var t = new Task();
             using (var context = new SystemDbContext())
             {
+                Project p = context.Projects.Find(SelectedProject.Id)!;
+                p.Tasks.Add(t);
                 context.Tasks.Add(t);
                 context.SaveChanges();
+                this.SelectedProject = p;
             }
-            //
+            
+            PopulateView();
 
         }
 
         private void PopulateView()
-        {
+        {   
+            // Querying from database.
             using (var context = new SystemDbContext())
             {
                 this.ProjectList = context.Projects.ToList<Project>();
-
-                if (ProjectList.Count != 0)
+                if (SelectedProject != null)
                 {
-                    this.SelectedProject = ProjectList.First();
                     this.TaskList = this.SelectedProject.Tasks.ToList<Task>();
                 }
-
+                else
+                {
+                    this.TaskList = new List<Task>();     
+                }
 
             }
+            
+            if (this.ProjectList.Count != 0)
+            {
+                BtnCreateTask.IsEnabled = true;
+                this.SelectedProject = ProjectList.Last();
+
+                if (this.TaskList.Count != 0)
+                {
+                    this.SelectedTask = TaskList.First();
+                    BtnAssignMaterial.IsEnabled = true;
+                }
+                else
+                {
+                    BtnAssignMaterial.IsEnabled = false;
+                }
+            }
+            else
+            { 
+                BtnCreateTask.IsEnabled = false;
+            }
+            
+            // Update DataGrids
             DgProjects.ItemsSource = null;
             DgTasks.ItemsSource = null;
             
             DgProjects.ItemsSource = this.ProjectList.ToList();
             DgTasks.ItemsSource = this.TaskList.ToList();
+            
+            // Move Focus
+            var lastProjectIndex = DgProjects.Items.Count - 1;
+            DgProjects.SelectedIndex = lastProjectIndex;
+            
+            DgProjects.UpdateLayout();
+            DgProjects.ScrollIntoView(DgProjects.Items[lastProjectIndex]);
+            BtnAssignMaterial.Content = this.SelectedProject.Id;
+            /**
+            var firstTaskIndex = 0;
+            DgTasks.SelectedIndex = firstTaskIndex;
+            
+            DgTasks.UpdateLayout();
+            DgTasks.ScrollIntoView(DgTasks.Items[firstTaskIndex]);
+            */
+            BtnAssignMaterial.Content = this.SelectedProject.Id + " " ;
+
+
 
         }
 
