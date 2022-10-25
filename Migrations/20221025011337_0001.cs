a7cb4b0f2796b695cba6e706820a111550c9278e
Lixxxo <luis.rojas04@alumnos.ucn.cl>
2022-10-24 22:16:21 -0300

    feat: change Datetime for Date

---
+++ Migrations/20221025011337_0001.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SistemaProyectos.Migrations
+{
+    public partial class _0001 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Hour",
+                table: "TaskMaterials",
+                newName: "Date");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Date",
+                table: "TaskMaterials",
+                type: "date",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Date",
+                table: "TaskMaterials",
+                newName: "Hour");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Hour",
+                table: "TaskMaterials",
+                type: "datetime",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "date",
+                oldNullable: true);
+        }
+    }
+}
